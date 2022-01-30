b0b818a3451c69399e829fdbfcbeee4d08cb6831
Christopher Peterson Sauer <christophersauer@pacbell.net>
2022-01-30 15:43:08 -0800

    Comment word smithing
    Just minor changes to save mental state

--- extract.py
+++ extract.py
@@ -195,10 +195,9 @@ def _all_platform_patch(compile_args: List[str]):
     # Seems to have disappeared when we switched to aquery from action_listeners, but we'll leave it in until the bug is patched in case we start using C++ modules
     compile_args = (arg for arg in compile_args if not arg.startswith('-fmodules-cache-path=bazel-out/'))
 
-    # When Bazel builds with gcc it adds -fno-canonical-system-headers to
-    # the command line, which clang does not understand.  See
-    # https://github.com/hedronvision/bazel-compile-commands-extractor/issues/21
-    # and https://github.com/clangd/clangd/issues/1004.
+    # When Bazel builds with gcc it adds -fno-canonical-system-headers to the command line, which clang tooling chokes on, since it does not understand this flag.
+    # We'll remove this flag, until such time as clangd & clang-tidy gracefully ignore it. Tracking issue: https://github.com/clangd/clangd/issues/1004.
+    # For more context see: https://github.com/hedronvision/bazel-compile-commands-extractor/issues/21
     compile_args = (arg for arg in compile_args if not arg == '-fno-canonical-system-headers')
 
     # Any other general fixes would go here...
