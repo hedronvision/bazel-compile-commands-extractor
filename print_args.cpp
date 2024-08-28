58b58c5f79961b3b04b0071cbc22ecd42ab62607
Simon Thornington <simon.thornington@gmail.com>
2024-08-28 15:39:06 -0400

    switch from iostream to stdio

--- print_args.cpp
+++ print_args.cpp
@@ -1,13 +1,13 @@
 // Prints the arguments passed to the script
 
-#include <iostream>
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  std::cout << "===HEDRON_COMPILE_COMMANDS_BEGIN_ARGS===\n";
-  for (int i = 1; i < argc; ++i) {
-    std::cout << argv[i] << "\n";
-  }
-  std::cout << "===HEDRON_COMPILE_COMMANDS_END_ARGS===\n";
-  // We purposely return a non-zero exit code to have the emcc process exit after running this fake clang wrapper.
-  return 1;
+    printf("===HEDRON_COMPILE_COMMANDS_BEGIN_ARGS===\n");
+    for (int i = 1; i < argc; ++i) {
+        printf("%s\n",argv[i]);
+    }
+    printf("===HEDRON_COMPILE_COMMANDS_END_ARGS===\n");
+    // We purposely return a non-zero exit code to have the emcc process exit after running this fake clang wrapper.
+    return 1;
 }
